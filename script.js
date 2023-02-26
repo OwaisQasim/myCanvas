6c6e2de91f3588b3deaba707791980b2f37b952b
iamrfowaisqasim <rfowaisqasim@gmail.com>
2023-02-26 14:13:19 +0500

    added the events so that we can draw circles and lines and now it is time to make the toolbox work

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-let size = 20
+let size = 10
 color = 'red'
+let isPressed = false
 let x
 let y
+
+canvas.addEventListener('mousedown', e => {
+    isPressed = true
+    x = e.offsetX
+    y = e.offsetY
+    drawCircle(x, y)
+})
+
+canvas.addEventListener('mouseup', () => {
+    isPressed = false
+    x = undefined
+    y = undefined
+})
+
+canvas.addEventListener('mousemove', e => {
+    if (isPressed) {
+        const x1 = e.offsetX
+        const y1 = e.offsetY
+        drawCircle(x1, y1)
+        drawLine(x, y, x1, y1)
+
+        x = x1
+        y = y1
+    }
+})
 
 function drawCircle(x, y) {
     ctx.beginPath()
@@ -18,7 +44,7 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size * 2
     ctx.stroke()
 }
 
